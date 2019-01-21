ebadd2a45bcb985f825215f5a61a395d88be4200
moriya takahiro <morley02160623@gmail.com>
2019-01-21 06:10:22 +0000

    物件登録index編集

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,7 +1,23 @@
 class PropertiesController < ApplicationController
   def index
+    @properties = Property.all
   end
   
   def new
+    @property = Property.new
+  end
+  
+  def create
+    Property.create(property_params)
+    redirect_to new_property_path
+  end
+  
+  def show
+  end
+  
+  private
+  
+  def property_params
+    params.require(:property).permit(:name, :city, :town, :postful_code, :adress, :after_adress, :rent, :floor_plan, :floor_space, :encount_monster, :stop_count, :stop_adress, :property_age,)
   end
 end
