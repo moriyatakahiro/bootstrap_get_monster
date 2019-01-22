15b67bf4b0d4abd6227f3b812e08e847e45acb46
moriya takahiro <morley02160623@gmail.com>
2019-01-22 10:54:26 +0000

    ログイン分岐完了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,11 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
       if @user && @user.authenticate(params[:session][:password])
         session[:user_id] = @user.id
-        redirect_to user_path(@user.id)
+        if @user.try(:admin?)
+          redirect_to properties_path
+        else
+          redirect_to user_path(@user.id)
+        end
       else
         flash.now[:danger] = 'ログインに失敗しました'
         render 'new'
