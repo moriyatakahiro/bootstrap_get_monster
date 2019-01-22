996d4c702cd97b9c18d98ffd0bad86ddde0e332a
moriya takahiro <morley02160623@gmail.com>
2019-01-22 10:44:08 +0000

    ログイン分岐実装中

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   
   namespace :admin do
-    resources :users, only: [:new, :create, :show]
+    resources :users
   end
   
   resources :users
