d6421b09f7aaf0b65fe552aaee5d42c1a202c259
moriya takahiro <morley02160623@gmail.com>
2019-01-22 07:59:07 +0000

    簡易ログイン機能

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+      if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user.id)
+      else
+        flash.now[:danger] = 'ログインに失敗しました'
+        render 'new'
+      end
+  end
+  
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
+end
+
+ private
+  
+  def login_check
+    @current_user = session[:user_id]
+    if @current_user.nil?
+      redirect_to new_session_path
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  namespace :admin do
+    resources :users, only: [:new, :create, :show]
+  end
+  
+  resources :users
+
   resources :properties do
     collection do
       post :confirm
