4312703ea6cf196bc6ee57728c534e0e7df8c44d
moriya takahiro <morley02160623@gmail.com>
2019-01-25 04:03:28 +0000

    お気に入り機能完了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   def create
     @favorite = current_user.favorites.create(property_id: params[:properties_id])
-    redirect_to users_path
+    redirect_to properties_url
   end
 
   def destroy
     @favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to users_path
+    redirect_to properties_url
   end
 end
