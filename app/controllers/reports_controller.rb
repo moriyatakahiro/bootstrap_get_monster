621767d4100f8625bcf4ef4b9800b884e07afb2e
moriya takahiro <morley02160623@gmail.com>
2019-02-01 07:21:09 +0000

    bootstrap実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,12 @@
 class ReportsController < ApplicationController
   def index
     @reports = Report.all
-    if params[:report][:search] == 'true'
-      @reports = Report.all.city_search
-    end
   end
-  
+
   def new
     @report = Report.new
   end
-  
+
   def create
     @report = Report.new(report_params)
     @report.user_id = current_user.id
@@ -19,18 +16,17 @@ class ReportsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def destroy
     @report = Report.find(params[:id])
     @report.destroy
     redirect_to reports_path
   end
-  
+
   private
-  
+
   def report_params
     params.require(:report).permit(:title, :content, :city, :town, :adress, :image, :image_cache)
   end
 end
 
- 
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,3 @@
 class Report < ApplicationRecord
     mount_uploader :image, ImageUploader
-    
-　　city_search, => {where("city LIKE ?" #全部一致する検索)
-　　
 end
